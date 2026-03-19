2b5625424b989da1e9fcddb120695aafd76ddbfd
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-03-19 19:14:09 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Header from './Components/Header'
 import Fūteris from './Components/Footer';
-import CardGrid from './Components/CardGrid';
 import Navigacija from './Components/Navigacija';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contacts from './Pages/Contacts';
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 
@@ -17,17 +16,19 @@ function App() {
 
   return (
     <>
-      <Navigacija></Navigacija>
-      <Header ></Header>
       <BrowserRouter>
+        <Navigacija></Navigacija>
+        <Header ></Header>
+      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contacts" element={<Contacts />} />
         </Routes>
 
-        </BrowserRouter >
-    <Fūteris></Fūteris>
+       
+      <Fūteris></Fūteris>
+     </BrowserRouter >
     </>
   )
 }
--- src/Components/Navigacija.jsx
+++ src/Components/Navigacija.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
 
 function Navigacija() {
   return (
@@ -11,19 +12,9 @@ function Navigacija() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={NavLink} to="/">Galvenā</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">Par mums</Nav.Link>
+            <Nav.Link as={NavLink} to="/contacts">Kontakti</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
